d53e8818e370f13e419bd582090bd01a1c595421
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2023-11-09 12:16:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-let startime = 0
+let startime = input.runningTime()
+while (input.runningTime() - startime < 60000) {
+    music.play(music.tonePlayable(input.runningTime() - startime, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(1000)
+}
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    startime = input.runningTime()
-    while (input.runningTime() - startime < 60) {
-        music.play(music.tonePlayable(input.runningTime() - startime, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        basic.showIcon(IconNames.Heart)
-        basic.showIcon(IconNames.SmallHeart)
-    }
-    basic.showIcon(IconNames.Yes)
+	
 })
